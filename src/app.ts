7074619fd9c19deb19d1d902806caa2ad22e6b2f
Dipra <dipradas5940@gmail.com>
2024-03-13 20:38:02 +0600

    12.9

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import { UserRoutes } from './app/modules/users/users.route'
 const app: Application = express()
@@ -13,5 +13,32 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/app/v1/users', UserRoutes)
+
+class ApiError extends Error {
+  statusCode: number
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  //   throw new ApiError(400, 'O re baba')
+  next('ore baba error')
+})
+
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err })
+  } else {
+    res.status(500).json({ error: 'Something went wrong.' })
+  }
+  console.log(err)
+})
 
 export default app
