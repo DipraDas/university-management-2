c2f369fc0e579632eb1fac23f8e88b2f5f8aa8ac
Dipra <dipradas5940@gmail.com>
2024-03-16 12:33:01 +0600

    13.4

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,8 +1,19 @@
 import { RequestHandler } from 'express-serve-static-core'
 import { NextFunction, Request, Response } from 'express'
 import { UserService } from './user.service'
+import { z } from 'zod'
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    const createUserZodSchema = z.object({
+      body: z.object({
+        role: z.string({
+          required_error: 'Role is required',
+        }),
+        password: z.string().optional(),
+      }),
+    })
+    await createUserZodSchema.parseAsync(req)
+
     const { user } = req.body
     const result = await UserService.createUser(user)
     res.status(200).json({
